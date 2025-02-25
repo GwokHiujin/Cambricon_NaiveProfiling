5d34040cff107ccb0470709dbd65ef48869b046b
GwokHiujin <guozi1625113688@163.com>
2025-02-25 14:25:32 +0800

    [Chore] Add CMakeLists

---
+++ bangC_test/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/test/naive_profiling/bangC_test
+# Build directory: /root/test/naive_profiling/bangC_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

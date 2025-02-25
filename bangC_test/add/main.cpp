0280e644900dfd243fa0f270e72545a6934ad8bf
GwokHiujin <guozi1625113688@163.com>
2025-02-25 17:46:05 +0800

    [Chore] Change host file to .cpp

--- bangC_test/add/main.cpp
+++ bangC_test/add/main.cpp
@@ -2,7 +2,11 @@
 #include <cstdlib>
 #include <cmath>
 
-#include "add.h"
+#include "cnrt.h"
+#include "../common.h"
+
+extern void AddKernel(float *output, const float *a,
+                   const float *b, int32_t data_num);
 
 float src1_cpu[ELEM_NUM];
 float src2_cpu[ELEM_NUM];
@@ -20,6 +24,7 @@ int main()
     cnrtDim3_t dim;
     cnrtFunctionType_t func_type;
     get_policy_function_block(&dim, &func_type);
+    dim.x = 16;
 
     for (int32_t i = 0; i < ELEM_NUM; ++i)
     {
@@ -30,6 +35,7 @@ int main()
     float *src1_mlu = NULL;
     float *src2_mlu = NULL;
     float *dst_mlu = NULL;
+    int32_t elements_num = ELEM_NUM / dim.x;
     CNRT_CHECK(cnrtMalloc((void **)&src1_mlu, ELEM_NUM * sizeof(float)));
     CNRT_CHECK(cnrtMalloc((void **)&src2_mlu, ELEM_NUM * sizeof(float)));
     CNRT_CHECK(cnrtMalloc((void **)&dst_mlu, ELEM_NUM * sizeof(float)));
@@ -39,16 +45,17 @@ int main()
     CNRT_CHECK(cnrtMemcpy(src2_mlu, src2_cpu, ELEM_NUM * sizeof(float),
                           cnrtMemcpyHostToDev));
 
+    void *args[] = {&dst_mlu, &src1_mlu, &src2_mlu, &elements_num};
     CNRT_CHECK(cnrtPlaceNotifier(notifier_start, queue));
-    AddKernel<<<dim, func_type, queue>>>(dst_mlu, src1_mlu, src2_mlu, ELEM_NUM);
+    CNRT_CHECK(cnrtInvokeKernel((void *)&AddKernel, dim, func_type, args, 0, queue));
     CNRT_CHECK(cnrtPlaceNotifier(notifier_end, queue));
     CNRT_CHECK(cnrtQueueSync(queue));
 
-    CNRT_CHECK(cnrtMemcpy(dst_cpu, dst_mlu, ELEM_NUM * sizeof(float),
-                          cnrtMemcpyDevToHost));
-
     float latency = 0.0;
     CNRT_CHECK(cnrtNotifierDuration(notifier_start, notifier_end, &latency));
+
+    CNRT_CHECK(cnrtMemcpy(dst_cpu, dst_mlu, ELEM_NUM * sizeof(float),
+                          cnrtMemcpyDevToHost));
 
     CNRT_CHECK(cnrtFree(src1_mlu));
     CNRT_CHECK(cnrtFree(src2_mlu));
@@ -68,7 +75,7 @@ int main()
     double io_bandwidth = get_io_bandwidth();
     double io_efficiency = theory_io / (latency * 1000) / io_bandwidth;
     double compute_efficiency = theory_ops / (latency * 1000) / peak_compute_force;
-    printf("[MLU Hardware Time ]: %.3f us\n", latency);
+    printf("[MLU Hardware Time ]: %.3f ms\n", latency / 1000);
     printf("[MLU IO Efficiency ]: %f\n", io_efficiency);
     printf("[MLU Compute Efficiency]: %f\n", compute_efficiency);
     printf("[MLU Diff Rate ]: %f\n", diff);
